e359baa1f903417f4e10dea495c285d8b70e1d05
thanhpppc1-boop <thanhpppc1@gmail.com>
2026-01-21 12:39:58 +0700

    Deploy to gh-pages

--- docusaurus.config.mjs
+++ docusaurus.config.mjs
@@ -4,196 +4,180 @@
  * Modify the docusaurus.config.js file at your site's root instead.
  */
 export default {
-  "title": "My Site",
-  "tagline": "Dinosaurs are cool",
-  "favicon": "img/favicon.ico",
-  "future": {
-    "v4": {
-      "removeLegacyPostBuildHeadAttribute": true,
-      "useCssCascadeLayers": true
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
+  future: {
+    v4: {
+      removeLegacyPostBuildHeadAttribute: true,
+      useCssCascadeLayers: true,
     },
-    "experimental_faster": {
-      "swcJsLoader": false,
-      "swcJsMinimizer": false,
-      "swcHtmlMinimizer": false,
-      "lightningCssMinimizer": false,
-      "mdxCrossCompilerCache": false,
-      "rspackBundler": false,
-      "rspackPersistentCache": false,
-      "ssgWorkerThreads": false
+    experimental_faster: {
+      swcJsLoader: false,
+      swcJsMinimizer: false,
+      swcHtmlMinimizer: false,
+      lightningCssMinimizer: false,
+      mdxCrossCompilerCache: false,
+      rspackBundler: false,
+      rspackPersistentCache: false,
+      ssgWorkerThreads: false,
     },
-    "experimental_storage": {
-      "type": "localStorage",
-      "namespace": false
+    experimental_storage: {
+      type: "localStorage",
+      namespace: false,
     },
-    "experimental_router": "browser"
+    experimental_router: "browser",
   },
-  "url": "https://your-docusaurus-site.example.com",
-  "baseUrl": "/",
-  "organizationName": "facebook",
-  "projectName": "docusaurus",
-  "onBrokenLinks": "throw",
-  "i18n": {
-    "defaultLocale": "en",
-    "locales": [
-      "en"
-    ],
-    "path": "i18n",
-    "localeConfigs": {}
+  url: "https://thanhpppc1-boop.github.io",
+  baseUrl: "/my-blog-b25/",
+  organizationName: "thanhpppc1-boop",
+  projectName: "my-blog-d25",
+  onBrokenLinks: "throw",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+    path: "i18n",
+    localeConfigs: {},
   },
-  "presets": [
+  presets: [
     [
       "classic",
       {
-        "docs": {
-          "sidebarPath": "./sidebars.js",
-          "editUrl": "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+        docs: {
+          sidebarPath: "./sidebars.js",
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        "blog": {
-          "showReadingTime": true,
-          "feedOptions": {
-            "type": [
-              "rss",
-              "atom"
-            ],
-            "xslt": true
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
           },
-          "editUrl": "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          "onInlineTags": "warn",
-          "onInlineAuthors": "warn",
-          "onUntruncatedBlogPosts": "warn"
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
-        "theme": {
-          "customCss": "./src/css/custom.css"
-        }
-      }
-    ]
-  ],
-  "themeConfig": {
-    "image": "img/docusaurus-social-card.jpg",
-    "colorMode": {
-      "respectPrefersColorScheme": true,
-      "defaultMode": "light",
-      "disableSwitch": false
-    },
-    "navbar": {
-      "title": "My Site",
-      "logo": {
-        "alt": "My Site Logo",
-        "src": "img/logo.svg"
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
       },
-      "items": [
-        {
-          "type": "docSidebar",
-          "sidebarId": "tutorialSidebar",
-          "position": "left",
-          "label": "Tutorial"
-        },
-        {
-          "to": "/blog",
-          "label": "Blog",
-          "position": "left"
-        },
-        {
-          "href": "https://github.com/facebook/docusaurus",
-          "label": "GitHub",
-          "position": "right"
-        }
-      ],
-      "hideOnScroll": false
+    ],
+  ],
+  themeConfig: {
+    image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      respectPrefersColorScheme: true,
+      defaultMode: "light",
+      disableSwitch: false,
     },
-    "footer": {
-      "style": "dark",
-      "links": [
+    navbar: {
+      title: "My Site",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
         {
-          "title": "Docs",
-          "items": [
-            {
-              "label": "Tutorial",
-              "to": "/docs/intro"
-            }
-          ]
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
         },
         {
-          "title": "Community",
-          "items": [
-            {
-              "label": "Stack Overflow",
-              "href": "https://stackoverflow.com/questions/tagged/docusaurus"
-            },
-            {
-              "label": "Discord",
-              "href": "https://discordapp.com/invite/docusaurus"
-            },
-            {
-              "label": "X",
-              "href": "https://x.com/docusaurus"
-            }
-          ]
+          to: "/blog",
+          label: "Blog",
+          position: "left",
         },
         {
-          "title": "More",
-          "items": [
-            {
-              "label": "Blog",
-              "to": "/blog"
-            },
-            {
-              "label": "GitHub",
-              "href": "https://github.com/facebook/docusaurus"
-            }
-          ]
-        }
+          href: "https://github.com/facebook/docusaurus",
+          label: "GitHub",
+          position: "right",
+        },
       ],
-      "copyright": "Copyright © 2026 My Project, Inc. Built with Docusaurus."
+      hideOnScroll: false,
     },
-    "prism": {
-      "theme": {
-        "plain": {
-          "color": "#393A34",
-          "backgroundColor": "#f6f8fa"
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
         },
-        "styles": [
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "X",
+              href: "https://x.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: "Copyright © 2026 My Project, Inc. Built with Docusaurus.",
+    },
+    prism: {
+      theme: {
+        plain: {
+          color: "#393A34",
+          backgroundColor: "#f6f8fa",
+        },
+        styles: [
           {
-            "types": [
-              "comment",
-              "prolog",
-              "doctype",
-              "cdata"
-            ],
-            "style": {
-              "color": "#999988",
-              "fontStyle": "italic"
-            }
+            types: ["comment", "prolog", "doctype", "cdata"],
+            style: {
+              color: "#999988",
+              fontStyle: "italic",
+            },
           },
           {
-            "types": [
-              "namespace"
-            ],
-            "style": {
-              "opacity": 0.7
-            }
+            types: ["namespace"],
+            style: {
+              opacity: 0.7,
+            },
           },
           {
-            "types": [
-              "string",
-              "attr-value"
-            ],
-            "style": {
-              "color": "#e3116c"
-            }
+            types: ["string", "attr-value"],
+            style: {
+              color: "#e3116c",
+            },
           },
           {
-            "types": [
-              "punctuation",
-              "operator"
-            ],
-            "style": {
-              "color": "#393A34"
-            }
+            types: ["punctuation", "operator"],
+            style: {
+              color: "#393A34",
+            },
           },
           {
-            "types": [
+            types: [
               "entity",
               "url",
               "symbol",
@@ -203,202 +187,159 @@ export default {
               "constant",
               "property",
               "regex",
-              "inserted"
-            ],
-            "style": {
-              "color": "#36acaa"
-            }
-          },
-          {
-            "types": [
-              "atrule",
-              "keyword",
-              "attr-name",
-              "selector"
-            ],
-            "style": {
-              "color": "#00a4db"
-            }
-          },
-          {
-            "types": [
-              "function",
-              "deleted",
-              "tag"
-            ],
-            "style": {
-              "color": "#d73a49"
-            }
-          },
-          {
-            "types": [
-              "function-variable"
-            ],
-            "style": {
-              "color": "#6f42c1"
-            }
-          },
-          {
-            "types": [
-              "tag",
-              "selector",
-              "keyword"
-            ],
-            "style": {
-              "color": "#00009f"
-            }
-          }
-        ]
-      },
-      "darkTheme": {
-        "plain": {
-          "color": "#F8F8F2",
-          "backgroundColor": "#282A36"
-        },
-        "styles": [
-          {
-            "types": [
-              "prolog",
-              "constant",
-              "builtin"
-            ],
-            "style": {
-              "color": "rgb(189, 147, 249)"
-            }
-          },
-          {
-            "types": [
               "inserted",
-              "function"
             ],
-            "style": {
-              "color": "rgb(80, 250, 123)"
-            }
+            style: {
+              color: "#36acaa",
+            },
           },
           {
-            "types": [
-              "deleted"
-            ],
-            "style": {
-              "color": "rgb(255, 85, 85)"
-            }
+            types: ["atrule", "keyword", "attr-name", "selector"],
+            style: {
+              color: "#00a4db",
+            },
           },
           {
-            "types": [
-              "changed"
-            ],
-            "style": {
-              "color": "rgb(255, 184, 108)"
-            }
+            types: ["function", "deleted", "tag"],
+            style: {
+              color: "#d73a49",
+            },
           },
           {
-            "types": [
-              "punctuation",
-              "symbol"
-            ],
-            "style": {
-              "color": "rgb(248, 248, 242)"
-            }
+            types: ["function-variable"],
+            style: {
+              color: "#6f42c1",
+            },
           },
           {
-            "types": [
-              "string",
-              "char",
-              "tag",
-              "selector"
-            ],
-            "style": {
-              "color": "rgb(255, 121, 198)"
-            }
+            types: ["tag", "selector", "keyword"],
+            style: {
+              color: "#00009f",
+            },
           },
-          {
-            "types": [
-              "keyword",
-              "variable"
-            ],
-            "style": {
-              "color": "rgb(189, 147, 249)",
-              "fontStyle": "italic"
-            }
-          },
-          {
-            "types": [
-              "comment"
-            ],
-            "style": {
-              "color": "rgb(98, 114, 164)"
-            }
-          },
-          {
-            "types": [
-              "attr-name"
-            ],
-            "style": {
-              "color": "rgb(241, 250, 140)"
-            }
-          }
-        ]
+        ],
       },
-      "additionalLanguages": [],
-      "magicComments": [
+      darkTheme: {
+        plain: {
+          color: "#F8F8F2",
+          backgroundColor: "#282A36",
+        },
+        styles: [
+          {
+            types: ["prolog", "constant", "builtin"],
+            style: {
+              color: "rgb(189, 147, 249)",
+            },
+          },
+          {
+            types: ["inserted", "function"],
+            style: {
+              color: "rgb(80, 250, 123)",
+            },
+          },
+          {
+            types: ["deleted"],
+            style: {
+              color: "rgb(255, 85, 85)",
+            },
+          },
+          {
+            types: ["changed"],
+            style: {
+              color: "rgb(255, 184, 108)",
+            },
+          },
+          {
+            types: ["punctuation", "symbol"],
+            style: {
+              color: "rgb(248, 248, 242)",
+            },
+          },
+          {
+            types: ["string", "char", "tag", "selector"],
+            style: {
+              color: "rgb(255, 121, 198)",
+            },
+          },
+          {
+            types: ["keyword", "variable"],
+            style: {
+              color: "rgb(189, 147, 249)",
+              fontStyle: "italic",
+            },
+          },
+          {
+            types: ["comment"],
+            style: {
+              color: "rgb(98, 114, 164)",
+            },
+          },
+          {
+            types: ["attr-name"],
+            style: {
+              color: "rgb(241, 250, 140)",
+            },
+          },
+        ],
+      },
+      additionalLanguages: [],
+      magicComments: [
         {
-          "className": "theme-code-block-highlighted-line",
-          "line": "highlight-next-line",
-          "block": {
-            "start": "highlight-start",
-            "end": "highlight-end"
-          }
-        }
-      ]
+          className: "theme-code-block-highlighted-line",
+          line: "highlight-next-line",
+          block: {
+            start: "highlight-start",
+            end: "highlight-end",
+          },
+        },
+      ],
     },
-    "docs": {
-      "versionPersistence": "localStorage",
-      "sidebar": {
-        "hideable": false,
-        "autoCollapseCategories": false
-      }
+    docs: {
+      versionPersistence: "localStorage",
+      sidebar: {
+        hideable: false,
+        autoCollapseCategories: false,
+      },
     },
-    "blog": {
-      "sidebar": {
-        "groupByYear": true
-      }
+    blog: {
+      sidebar: {
+        groupByYear: true,
+      },
     },
-    "metadata": [],
-    "tableOfContents": {
-      "minHeadingLevel": 2,
-      "maxHeadingLevel": 3
-    }
+    metadata: [],
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 3,
+    },
   },
-  "baseUrlIssueBanner": true,
-  "onBrokenAnchors": "warn",
-  "onDuplicateRoutes": "warn",
-  "staticDirectories": [
-    "static"
-  ],
-  "customFields": {},
-  "plugins": [],
-  "themes": [],
-  "scripts": [],
-  "headTags": [],
-  "stylesheets": [],
-  "clientModules": [],
-  "titleDelimiter": "|",
-  "noIndex": false,
-  "markdown": {
-    "format": "mdx",
-    "mermaid": false,
-    "emoji": true,
-    "mdx1Compat": {
-      "comments": true,
-      "admonitions": true,
-      "headingIds": true
+  baseUrlIssueBanner: true,
+  onBrokenAnchors: "warn",
+  onDuplicateRoutes: "warn",
+  staticDirectories: ["static"],
+  customFields: {},
+  plugins: [],
+  themes: [],
+  scripts: [],
+  headTags: [],
+  stylesheets: [],
+  clientModules: [],
+  titleDelimiter: "|",
+  noIndex: false,
+  markdown: {
+    format: "mdx",
+    mermaid: false,
+    emoji: true,
+    mdx1Compat: {
+      comments: true,
+      admonitions: true,
+      headingIds: true,
     },
-    "anchors": {
-      "maintainCase": false
+    anchors: {
+      maintainCase: false,
     },
-    "hooks": {
-      "onBrokenMarkdownLinks": "warn",
-      "onBrokenMarkdownImages": "throw"
-    }
-  }
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+      onBrokenMarkdownImages: "throw",
+    },
+  },
 };
